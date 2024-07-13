1193623d48f4fc6483c291a6ee9d31dfe8fb6926
coldstone <anil@myarch.linux>
2024-07-13 08:57:24 +0530

    adding neotree and treesitter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,9 +4,6 @@ local function map(mode, lhs, rhs)
 	vim.keymap.set(mode, lhs, rhs, { silent = true })
 end
 
--- Save
-map("n", "<leader>w", "<CMD>update<CR>")
-
 -- Quit
 map("n", "<leader>q", "<CMD>q<CR>")
 
